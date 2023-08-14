3f2059cf08bb4f9952842e08397d03a089768637
Allen <zqj1130123899@gmail.com>
2023-08-14 11:41:02 +0800

    wip: build blog

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,25 +2,44 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Dog Man",
-  // base: "/ZBlog/",
-  description: "dmZhan's station",
+  lang: 'zh-CN',
+  title: "Dog Man's blog",
+  description: "Dog Man's blog",
+  lastUpdated: true,
+  cleanUrls: true,
+  // appearance: false,
+  markdown: {
+    lineNumbers: true
+  },
+  buildEnd: ()=>console.log(123),
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    logo: { src: '/horse.svg' },
+    outline: [2, 3],
+    search: {
+      provider: 'local'
+    },
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '🏡Home', link: '/' },
+      {
+        text: "🔖Tags",
+        link: "/Tags",
+      },
+      {
+        text: "📃Archives",
+        link: "/Archives",
+      },
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
