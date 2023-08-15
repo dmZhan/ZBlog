077a2fa8a6edf0355bbf51cfbcf171ef6fd680b8
Allen <zqj1130123899@gmail.com>
2023-08-15 21:08:04 +0800

    feat: use giscus

--- docs/.vitepress/theme/components/Layout/Comments.vue
+++ docs/.vitepress/theme/components/Layout/Comments.vue
@@ -1,30 +1,31 @@
 <!-- 文章评论区域 -->
 <script setup lang="ts">
-// import { useData } from 'vitepress'
+import { useData } from 'vitepress'
 
-// const { title } = useData()
+const { title } = useData()
 </script>
 
 <template>
   <div class="comments">
-    need to do something
-    <!-- <component
+    <!-- need to do something -->
+    <component
       :is="'script'"
       :key="title"
       src="https://giscus.app/client.js"
       data-repo="dmZhan/ZBlog"
-      data-repo-id="R_kgDOJQkw9Q"
+      data-repo-id="R_kgDOKElQvg"
       data-category="General"
-      data-category-id="DIC_kwDOJQkw9c4CVeXc"
+      data-category-id="DIC_kwDOKElQvs4CYnjA"
       data-mapping="pathname"
       data-strict="0"
       data-reactions-enabled="1"
-      data-emit-metadata="0"
+      data-emit-metadata="1"
       data-input-position="top"
+      data-theme="preferred_color_scheme"
       data-lang="en"
-      data-theme="transparent_dark"
       data-loading="lazy"
+      crossorigin="anonymous"
       async
-    /> -->
+    />
   </div>
 </template>
